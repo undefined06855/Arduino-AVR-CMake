d207d97f4935348896571bace7e3fcfcae87a635
undefined06855 <currentcashew57@gmail.com>
2026-02-10 21:42:37 +0000

    update and fix paths

--- cmake/toolchain/avr.toolchain.cmake
+++ cmake/toolchain/avr.toolchain.cmake
@@ -21,11 +21,10 @@ set(ARDUINO_VERSION "10815" CACHE STRING
     "Arduino IDE version (used for the macro with the same name)")
 
 # Derived paths
-set(ARDUINO_AVR_PATH ${ARDUINO_PATH}/hardware/avr/${ARDUINO_CORE_VERSION})
-set(ARDUINO_CORE_PATH ${ARDUINO_AVR_PATH}/cores/arduino)
-set(ARDUINO_TOOLS_PATH ${ARDUINO_PATH}/tools/avr-gcc/${AVR_GCC_VERSION}/bin)
-set(ARDUINO_AVRDUDE_PATH ${ARDUINO_PATH}/tools/avrdude/${AVRDUDE_VERSION})
-set(ARDUINO_AVRDUDE_CONF ${ARDUINO_AVRDUDE_PATH}/etc/avrdude.conf)
+set(ARDUINO_AVR_PATH ${ARDUINO_PATH}/hardware/arduino/avr)
+set(ARDUINO_CORE_PATH ${ARDUINO_PATH}/hardware/arduino/avr/cores/arduino)
+set(ARDUINO_TOOLS_PATH ${ARDUINO_PATH}/hardware/tools/avr/bin)
+set(ARDUINO_AVRDUDE_CONF ${ARDUINO_PATH}/hardware/tools/avr/etc/avrdude.conf)
 
 # Toolchain paths
 find_program(CMAKE_C_COMPILER avr-gcc PATHS "${ARDUINO_TOOLS_PATH}" NO_DEFAULT_PATH
@@ -36,7 +35,7 @@ find_program(CMAKE_OBJCOPY avr-objcopy PATHS "${ARDUINO_TOOLS_PATH}" NO_DEFAULT_
     DOC "Path to avr-objcopy")
 find_program(CMAKE_SIZE avr-size PATHS "${ARDUINO_TOOLS_PATH}" NO_DEFAULT_PATH
     DOC "Path to avr-size")
-find_program(ARDUINO_AVRDUDE avrdude PATHS "${ARDUINO_AVRDUDE_PATH}/bin" NO_DEFAULT_PATH
+find_program(ARDUINO_AVRDUDE avrdude PATHS "${ARDUINO_TOOLS_PATH}" NO_DEFAULT_PATH
     DOC "Path to avrdude")
 
 # Only look libraries etc. in the sysroot, but never look there for programs
